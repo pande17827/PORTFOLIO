bd05b7b653c634187a5a70b5c1cd04b543d624f9
Vikas Kumar <45943546+pande17827@users.noreply.github.com>
2025-04-15 13:04:48 +0000

    pm

--- src/components/robotmodel/RobotModel.jsx
+++ src/components/robotmodel/RobotModel.jsx
@@ -3,10 +3,10 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
 export default function RobotModel() {
-  const { scene } = useGLTF("/flying_robot_07___07.glb"); // ✅ Correct path from public/
+  const { scene } = useGLTF("src/assets/flying_robot_07___07.glb"); // Ensure path is correct
 
   return (
-    <Canvas style={{ height: "300px", width: "200px" }}>
+    <Canvas style={{ height: "300px",width:"200px" }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[2, 5, 2]} intensity={1} />
       <Suspense fallback={null}>
